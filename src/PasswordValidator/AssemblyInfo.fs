4e5e4f74a716fad5db813bbf11f8efd87a6334c5
Tomas Petricek <tomas@tomasp.net>
2014-11-18 23:41:08 +0100

    initial commit

---
+++ src/PasswordValidator/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("PasswordValidator")>]
+[<assembly: AssemblyProductAttribute("PasswordValidator")>]
+[<assembly: AssemblyDescriptionAttribute("Simple password validator")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
