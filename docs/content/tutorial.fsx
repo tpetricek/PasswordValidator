38c4910a543aeff5326ae92debffdb5c50b980b5
Tomas Petricek <tomas@tomasp.net>
2014-11-19 11:41:37 +0100

    Bump version to 1.3

--- docs/content/tutorial.fsx
+++ docs/content/tutorial.fsx
@@ -2,7 +2,52 @@
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
 #I "../../bin"
-
 (**
-Demo
+Password validator tutorial
+===========================
+
+This is a silly demo library for validating passwords. You can check
+that password has the specified _length_, contains _upper case_ letter
+and contains _digits_.
+
+Referencing the library
+-----------------------
+
+If you're using F# Interactive, you can reference the library as follows:
 *)
+#r "PasswordValidator.dll"
+open PasswordValidator
+(**
+Validating passwords
+--------------------
+
+There are two ways of validating passwords:
+
+ * **Requirements** - You can use individual requirements via
+   the `IRequirement` interface. The implementations can be found
+   in the `Validators` module.
+
+ * **Power validator** - Alternatively, you can use the 
+   `PowerValidator` type, which takes a collection of requirements.
+
+### Requirements
+
+The following snippet demonstrates the length requirement:
+*)
+let len = Validators.LengthRequirement
+len.IsSatisfied("hello")       // 'false'
+len.IsSatisfied("hello world") // 'true'
+(**
+
+### Power validator
+
+The following demonstrates how to use `PowerValidator` to 
+check for the presence of digit and uppr case letter:
+*)
+let valids = 
+  [ Validators.DigitRequirement 
+    Validators.UpperCaseRequirement ]
+
+let pow = PowerValidator(valids)
+pow.IsSatisfied("Hello")  // 'false'
+pow.IsSatisfied("1Hello") // 'true'
--- src/PasswordValidator/Library.fs
+++ src/PasswordValidator/Library.fs
@@ -1,26 +1,40 @@
 namespace PasswordValidator
 
+/// Defines a password requirement
 type IRequirement = 
+  /// Returns true if the password is OK
   abstract IsSatisfied : string -> bool  
   
+/// The `Validators` module provides access to the individual
+/// password validators. Those can be used to check simple 
+/// password conditions, including the following:
+///
+///  - **Length** - check that password is at least 8 characters
+///  - **Digits** - check that password contains a digit
+///  - **Upper case** - check that password includes upper case letters
+///
 module Validators =
+  /// Checks length
   let LengthRequirement = 
     { new IRequirement with
         member x.IsSatisfied(password) = 
           password.Length >= 8 }
 
+  /// Checks digits
   let DigitRequirement =
     { new IRequirement with
         member x.IsSatisfied(password) = 
           password 
           |> Seq.exists System.Char.IsDigit }
   
+  /// Checks length
   let UpperCaseRequirement =
     { new IRequirement with
         member x.IsSatisfied(password) = 
           password 
           |> Seq.exists System.Char.IsUpper }
 
+/// Checks lots of stuff
 type PowerValidator(requirements:seq<IRequirement>) = 
   member x.IsSatisfied(password) =
     requirements 
