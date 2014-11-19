c065d329f580e48df4bd8c14dee4f76d6d52f489
Tomas Petricek <tomas@tomasp.net>
2014-11-19 09:53:56 +0100

    Bump version to 1.3

--- src/PasswordValidator/AssemblyInfo.fs
+++ src/PasswordValidator/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("PasswordValidator")>]
 [<assembly: AssemblyProductAttribute("PasswordValidator")>]
 [<assembly: AssemblyDescriptionAttribute("Simple password validator")>]
-[<assembly: AssemblyVersionAttribute("1.2")>]
-[<assembly: AssemblyFileVersionAttribute("1.2")>]
+[<assembly: AssemblyVersionAttribute("1.3")>]
+[<assembly: AssemblyFileVersionAttribute("1.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.2"
+    let [<Literal>] Version = "1.3"
